cee65314272ef04c953d2f4b0a161ff7c44c3df9
Cookie <rody.jansen@gmail.com>
2024-06-03 12:40:35 +0200

    init commit: gardener

---
+++ Gardener/Gardener/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Gardener
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gardener/Gardener/Plant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gardener;
+
+public class Plant
+{
+    public string Name { get; set; };
+    public PlantColor Color { get; set; };
+}
