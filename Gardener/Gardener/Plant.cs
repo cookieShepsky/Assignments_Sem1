c84dceecd83b1f42cf3577d4180886c183d8b828
Cookie <rody.jansen@gmail.com>
2024-06-03 13:06:31 +0200

    feat Plant.cs: properties

--- Gardener/Gardener/Plant.cs
+++ Gardener/Gardener/Plant.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace Gardener;
 
-public class Plant
+public class Plant()
 {
     public string Name { get; private set; }
-    public PlantColor Color { get; private set; }
+    public string Color { get; private set; }
     public bool Evergreen { get; private set; }
     public PlantType Type { get; private set; }
-    public List<TimeSpan> BlossomPeriods { get; private set; }  //.count should be >0
-    public List<TimeSpan> PrunePeriods { get; private set; }    //.count should be >0
+    public List<TimeSpan> BlossomPeriods { get; private set; } = [];    //.count should be >0
+    public List<TimeSpan> PrunePeriods { get; private set; } = [];      //.count should be >0
 }
