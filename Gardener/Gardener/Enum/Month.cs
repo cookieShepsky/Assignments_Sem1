44ad2dad9d0e35acc82ea5dfbe6db4b3bbab4e0c
Cookie <rody.jansen@gmail.com>
2024-06-03 13:14:53 +0200

    create month.cs: enum

---
+++ Gardener/Gardener/Enum/Month.cs
@@ -0,0 +1,17 @@
+﻿namespace Gardener;
+
+public enum Month
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
