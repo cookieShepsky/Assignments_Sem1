4234ec664c51554bdabee3f05eaa0e150e099e99
Cookie <rody.jansen@gmail.com>
2024-06-03 12:55:08 +0200

    feat Plant.cs: Properties

--- Gardener/Gardener/Plant.cs
+++ Gardener/Gardener/Plant.cs
@@ -8,6 +8,10 @@ namespace Gardener;
 
 public class Plant
 {
-    public string Name { get; set; };
-    public PlantColor Color { get; set; };
+    public string Name { get; private set; }
+    public PlantColor Color { get; private set; }
+    public bool Evergreen { get; private set; }
+    public PlantType Type { get; private set; }
+    public List<TimeSpan> BlossomPeriods { get; private set; }  //.count should be >0
+    public List<TimeSpan> PrunePeriods { get; private set; }    //.count should be >0
 }
