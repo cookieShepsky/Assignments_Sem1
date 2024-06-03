d7a92f22030d9af42fc217a86aef3dbecf865b1d
Cookie <rody.jansen@gmail.com>
2024-06-03 13:56:54 +0200

    feat Plant.cs: complete

--- Gardener/Gardener/Plant.cs
+++ Gardener/Gardener/Plant.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gardener;
+﻿namespace Gardener;
 
 public class Plant
 {
-    public string Name { get; private set; }                                   // should != ""
+    public string Name { get; private set; }
     public string Color { get; private set; }
     public bool Evergreen { get; private set; }
     public PlantType Type { get; private set; }
-    public List<(Month, Month)> BlossomPeriods { get; private set; } = [];    //.count should be >0
-    public List<(Month, Month)> PrunePeriods { get; private set; } = [];      //.count should be >0
+    public List<(Month, Month)> BlossomPeriods { get; private set; } = [];
+    public List<(Month, Month)> PrunePeriods { get; private set; } = [];
 
     public Plant(string name, string color, bool evergreen, PlantType type)
     {
@@ -24,10 +17,15 @@ public class Plant
         this.Type = type;
     }
 
-    public void AddBlossomPeriod((Month, Month) period)
-    {
-        BlossomPeriods.Add(period);
-    }
+    /// <summary>
+    /// Adds a period to this plant's BlossomPeriods
+    /// </summary>
+    public void AddBlossomPeriod((Month, Month) period) { BlossomPeriods.Add(period); }
+
+    /// <summary>
+    /// Adds a period to this plant's PrunePeriods
+    /// </summary>
+    public void AddPrunePeriod((Month, Month) period) { PrunePeriods.Add(period); }
 
     /// <summary>
     /// Returns a list of enums indicating what's wrong with the object.
